c8e8dc5e7721a5f5df58f1f1cd8775db6c296e5f
owenglahn <orglahn@gmail.com>
2020-12-22 15:36:05 -0500

    Revert "Knapsack 1st"
    This reverts commit a6d80a5d5319537d701c741651d628a453ad9f98.

--- US_elections.java
+++ US_elections.java
@@ -4,8 +4,7 @@ import java.io.FileNotFoundException;
 
 public class US_elections {
 
-	public static int solution(int num_states, int[] delegates, int[] votes_Biden, int[] votes_Trump,
-			int[] votes_Undecided) {
+	public static int solution(int num_states, int[] delegates, int[] votes_Biden, int[] votes_Trump, int[] votes_Undecided) {
 		int[] votesNeededBS = getVotesNeededBS(num_states, votes_Biden, votes_Trump, votes_Undecided);
 		int trumpDels = 0; // delegates guaranteed to be won by trump
 		int bidenDels = 0; // delegates winnable by biden
@@ -40,8 +39,7 @@ public class US_elections {
 				votesNeededBS[i] = (int) (votesBehind + Math.ceil((votes_Undecided[i] - votesBehind) / 2.0));
 			} else {
 				votesNeededBS[i] = (int) Math.ceil((votes_Undecided[i] + votesBehind) / 2.0);
-				if (votesNeededBS[i] < 0)
-					votesNeededBS[i] = 0; // Biden is guaranteed to win state
+				if ( votesNeededBS[i] < 0 ) votesNeededBS[i] = 0; // Biden is guaranteed to win state
 			}
 			if (votesNeededBS[i] + votes_Biden[i] == votes_Trump[i] + votes_Undecided[i] - votesNeededBS[i]) {
 				if (votesNeededBS[i] == votes_Undecided[i])
@@ -52,43 +50,38 @@ public class US_elections {
 		}
 		return votesNeededBS;
 	}
-
+	
 	/*
 	 * Helper method to remove states unnecessary to winning from calculation.
 	 */
-	public static int clean(int num_states, int bidenDels, int votesNeeded, int[] delegates, int[] votesNeededBS) {
+	public static int clean(int num_states, int bidenDels, int votesNeeded, int[] delegates, int[] votesNeededBS)
+	{
 		int totalDels = 0;
-		for (int i = 0; i < num_states; i++) {
+		List<State> winnable = new ArrayList<>();
+		for ( int i = 0 ; i < num_states ; i++ )
+		{
+			if (votesNeededBS[i] > 0) winnable.add(new State(delegates[i], votesNeededBS[i]));
 			totalDels += delegates[i];
 		}
-		int threshHold = totalDels / 2 + 1; // dels needed to win
-		if (bidenDels == threshHold)
-			return votesNeeded; // save time
-		int capacity = bidenDels - threshHold; // max weight for the knapsack problem
+//		Collections.sort(winnable);
+		int threshHold = totalDels/2; // dels needed to win
+		if (bidenDels == threshHold + 1) return votesNeeded; // save time
+//		for (State state : winnable) 
+//		{
+//			if (bidenDels - state.delegates > threshHold)
+//			{
+//				votesNeeded -= state.votesNeeded;
+//				bidenDels -= state.delegates;
+//			}
+//		}
+		int[] M = new int[num_states];
+		if (votesNeededBS[0] != -1 && votesNeeded - votesNeededBS[0] >= threshHold) M[0] = votesNeeded - votesNeededBS[0];
+		else M[0] = votesNeeded;
+		for (int i = 1; i < num_states; i++)
+		{
+		}
 		
-		/* 
-		 * Usage of the knapsack problem where the number of delegates is the weight 
-		 * and the number of votes needed to win is the value. 
-		 */
-		int[][] M = new int[num_states + 1][capacity + 1];
-		for (int w = 0 ; w <= capacity; w++)
-		{
-			M[0][w] = 0;
-		}
-		for (int i = 1; i <= num_states; i++)
-		{
-			M[i][0] = 0;
-			for (int w = 1; w <= capacity; w++)
-			{
-				if (delegates[i-1] > w) M[i][w] = M[i-1][w];
-				else M[i][w] = Math.max(M[i-1][w], votesNeededBS[i-1] + M[i - 1][w - delegates[i-1]]);
-			}
-		}
-		for (int i = 0 ; i < M.length; i++)
-		{
-			System.out.println(Arrays.toString(M[i]));
-		}
-		return M[num_states][bidenDels - threshHold];
+		return votesNeeded;
 	}
 
 	public static void main(String[] args) {
@@ -114,5 +107,38 @@ public class US_elections {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+	}
+}
+
+/*
+ * Helper class representing a state.
+ * 
+ * Used in clean() method to be able to sort states in order of the votes needed to win them.
+ */
+class State implements Comparable<State>
+{
+	int delegates; // delegates awarded by that state
+	int votesNeeded; // votes needed by biden to win state
+
+	State(int pDels, int pVotes)
+	{
+		delegates = pDels;
+		votesNeeded = pVotes;
+	}
+	
+	/*
+	 * Sort in order of descending votes needed
+	 */
+	@Override
+	public int compareTo(State o) {
+		return o.votesNeeded - votesNeeded;
+	}
+	
+	/*
+	 * Used for testing
+	 */
+	public String toString()
+	{
+		return "(" + delegates + ":" + votesNeeded + ")";
 	}
 }
